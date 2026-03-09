e18c5fb817cb75605bc37da1f07019c062a11b30
Abhishek Adhikari <16abhiadhi@gmail.com>
2026-03-09 18:11:18 +0530

    Update terraform.tf

--- terraform/project/terraform.tf
+++ terraform/project/terraform.tf
@@ -1,8 +1,16 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "6.35.1"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"         
     }
+  }
+
+  backend "s3" {
+    bucket         = "dev-infra-app-bucket-abhishek-dev-2026"
+    key            = "terraform/state/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "dev-infra_project"
+    encrypt        = true
   }
 }
